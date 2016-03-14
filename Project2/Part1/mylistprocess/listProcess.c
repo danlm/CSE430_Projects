7fe945fbe9af4966eb0bb2bb2220ae6726dbaa8c
D Martin <dlmart11@asu.edu>
2016-03-13 18:15:10 -0700

    Implemented syscall according to pseudocode

--- Project2/Part1/mylistprocess/listProcess.c
+++ Project2/Part1/mylistprocess/listProcess.c
@@ -1,3 +1,4 @@
+// CSE 430 Project 2
 /** The following template is the kernel space C program that you may use. Fill in the necessary code and try to compile it. Make sure you read the reference given in the hints to fully understand the given code.
  */
 
@@ -14,33 +15,65 @@
 struct task_info
 {
 	//Create a variable to store the PID
+	int PID;
+	
     //Create a variable to store the tty
+    char tty[64];
+    
     //Create a variable to store the time
+    unsigned long long time;
+    
     //Create a variable to store the command that started the process
+    char cmd[16];
 };
 
 /** This function generates the required task list and send it to user space. */
 static void generate_Tasklist(void *buf, int size)
 {
 	// Create a temporary structure variable of type task_info
+	struct task_info temp;
+	
 	// Create a pointer of type task_struct
-    // You will need to use a variable to iterate through the buffer that stores the information
+	struct task_struct *task;
+	
+	// You will need to use a variable to iterate through the buffer that stores the information
 	read_lock(&tasklist_lock); // This will lock the overall task list
 	for_each_process(task)
-    {
+	{
 		// Lock each task before you do any reading from it
+		read_lock(&task);
+		
 		// Extract each of the 4 fields needed - PID, tty, time and command from the task and into the temp variable of type task_info
+		temp.PID = task->PID;
+		if(task->signal == NULL || task->signal->tty == NULL)
+		{
+			strncpy(temp.tty, "?", 64);
+		}
+		else {
+			strncpy(temp.tty, task->signal->tty->name, 64);
+		}
+		temp.time = task->time;
+		strncpy(temp.cmd, task->comm, 16);
+		
 		// Unlock the task
+		read_unlock(&task);
+		
 		// Copy the contents of the temp variable into the variable buf using the right command
+		copy_to_user(buf, &temp, sizeof(temp));
+		
 		// You may print the values you extracted to the kernel log just to be sure
+		printk(KERN_INFO "%d\t%s\t%d\t%s\n", temp.pid, temp.tty, temp.time, temp.cmd);
 	}
-	// Unlcok the overall task list
+	// Unlock the overall task list
+	read_unlock(&tasklist_lock);
 }
 
 /** This is the system call. */
-asmlinkage long sys_mylistprocess(char *buffer,int size)
+asmlinkage long sys_mylistprocess(char *buffer, int size)
 {
 	// Call generate_Tasklist() with the appropriate arguments and return the right value.
+	generate_Tasklist(buf, size);
+	return 0;
 }
 
 
--- Project2/Part1/mylistprocess/test_mycall.c
+++ Project2/Part1/mylistprocess/test_mycall.c
@@ -1,8 +1,8 @@
-/*	CSE 430 Assignment 1
+/*	CSE 430 Project 2
 test_mycall.c
 Description: User-space program to test a system call.
 */
-
+#include <stdio.h>
 #include <linux/unistd.h>
 #define __NR_my_syscall 359
 
@@ -10,13 +10,24 @@ Description: User-space program to test a system call.
 struct task_info
 {
 	//Create a variable to store the PID
+	int PID;
+	
     //Create a variable to store the tty
+    char tty[64];
+    
     //Create a variable to store the time
+    unsigned long long time;
+    
     //Create a variable to store the command that started the process
+    char cmd[16];
 };
 
 int main()
 {
-	syscall(__NR_my_syscall);
+	char* buffer;
+	int size = 1024;
+	syscall(__NR_my_syscall, buffer, size);
+	printf("PID\tTTY\tTIME\tCMD\n");
+	
 	return 0;
 }
